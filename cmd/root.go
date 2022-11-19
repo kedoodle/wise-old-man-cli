93c92b06490f362af9295a4d1483dec394f616f0
Keddy Shen <hello@keddy.dev>
2022-11-20 00:11:19 +1100

    update root command description

--- cmd/root.go
+++ cmd/root.go
@@ -8,13 +8,9 @@ import (
 
 var rootCmd = &cobra.Command{
 	Use:   "wise-old-man-cli",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Talk-to Wise Old Man",
+	Long: `Talk-to Wise Old Man
+Command Dionysius to do your bidding.`,
 }
 
 func Execute() {
