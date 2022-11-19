f3bd8d0463a3d3aa86b404ffb02300573ff816c2
Keddy Shen <hello@keddy.dev>
2022-11-20 10:04:16 +1100

    update players synchronously

--- cmd/update_players.go
+++ cmd/update_players.go
@@ -2,6 +2,9 @@ package cmd
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+	"net/url"
 
 	"github.com/spf13/cobra"
 )
@@ -12,6 +15,19 @@ var playersCmd = &cobra.Command{
 	Args:  cobra.MinimumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Printf("updating players: %+q\n", args)
+		client := http.Client{}
+		for _, username := range args {
+			url := url.URL{
+				Scheme: "https",
+				Host:   "api.wiseoldman.net",
+				Path:   fmt.Sprintf("/v2/players/%s", username),
+			}
+			_, err := client.Post(url.String(), "application/json", nil)
+			if err != nil {
+				log.Printf("error updating player %q: %v", username, err)
+			}
+			log.Printf("updated player %q", username)
+		}
 	},
 }
 
